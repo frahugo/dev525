ddc8718876650e6eace2b921a2ad0bfacf03bd21
Hugo Frappier <hugo@cekoya.com>
2011-07-13 10:42:06 -0400

    Il manquait l'index
    Signed-off-by: Hugo Frappier <hugo@cekoya.com>

--- solutions/lab7/vinier/db/migrate/20110708181337_add_wine_type_id_to_wine.rb
+++ solutions/lab7/vinier/db/migrate/20110708181337_add_wine_type_id_to_wine.rb
@@ -1,9 +1,11 @@
 class AddWineTypeIdToWine < ActiveRecord::Migration
   def self.up
     add_column :wines, :wine_type_id, :integer
+    add_index :wines, :wine_type_id
   end
 
   def self.down
+    remove_index :wines, :wine_type_id
     remove_column :wines, :wine_type_id
   end
 end
